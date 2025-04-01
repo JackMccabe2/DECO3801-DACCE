f335a4a67eee553db3bb4b1aecd1f5195c67ea12
Wen Kao <chiawenkao23@gmail.com>
2025-04-01 19:19:03 +1000

    Update playgame page

--- Frontend_React/dacce/src/views/playGame.jsx
+++ Frontend_React/dacce/src/views/playGame.jsx
@@ -11,28 +11,30 @@ import "../css/playGame.css";
 const playGame = () => {
   return (
     <>
-      <Container>
+      <div className="dashboard-wrap">
         <Header />
-        <Row>
-          <Col sm={12} md={5} lg={5} className="custom-playmode-container">
-            <div>
-              <h4 className="custom-title">PvP</h4>
-            </div>
-            <div>
-              <Button text="Find Match" colour="yellow"></Button>
-            </div>
-            <Col sm={12} md={2} lg={2}></Col>
-            <div>
-              <Button text="Invite" colour="orange"></Button>
-            </div>
-          </Col>
-          <Col sm={12} md={5} lg={5} className="custom-playmode-container">
-            <h4 className="custom-title">PvC</h4>
-            <Button text="Single Player" colour="yellow"></Button>
-            <Button text="Multi Player" colour="yellow"></Button>
-          </Col>
-        </Row>
-      </Container>
+        <Container className="mt-5">
+          <Row className="d-flex justify-content-center gap-5">
+            <Col sm={12} md={5} lg={5} className="custom-playmode-container">
+              <div>
+                <h4 className="custom-title">PvP</h4>
+              </div>
+              <div>
+                <Button text="Find Match" colour="yellow"></Button>
+              </div>
+              <Col sm={12} md={2} lg={2}></Col>
+              <div>
+                <Button text="Invite" colour="orange"></Button>
+              </div>
+            </Col>
+            <Col sm={12} md={5} lg={5} className="custom-playmode-container">
+              <h4 className="custom-title">PvC</h4>
+              <Button text="Single Player" colour="yellow"></Button>
+              <Button text="Multi Player" colour="yellow"></Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 };
