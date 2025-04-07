657ccaf9b308caa5ced022590c7d852de8fa485d
eltachou <SD125715@gmail.com>
2025-04-07 23:26:14 +1000

    Added burger menu, updated header view change and added into App.jsx

--- Frontend_React/dacce/src/App.jsx
+++ Frontend_React/dacce/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Landing from "./views/landing";
 import Background from "../src/components/background";
@@ -8,12 +8,13 @@ import PlayGame from "./views/playGame";
 
 import Signup from "./views/signup";
 import Login from "./views/login";
+import Header from "./components/header";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  // Set different views for the app.
   const [currentView, setCurrentView] = useState("landing");
-
   // Do nothing if current view is landing, otherwise set new view.
   const renderView = (view) => {
     if (view === currentView) {
@@ -25,6 +26,12 @@ function App() {
   return (
     <>
       <div className="view-container">
+        {/* Only show header when views are not any of the following */}
+        {currentView !== "landing" &&
+          currentView !== "signup" &&
+          currentView !== "login" && (
+            <Header currentView={currentView} onNavigate={renderView} />
+          )}
         {currentView === "landing" && <Landing onNavigate={renderView} />}
         {currentView === "signup" && <Signup onNavigate={renderView} />}
         {currentView === "login" && <Login onNavigate={renderView} />}
--- Frontend_React/dacce/src/components/header.jsx
+++ Frontend_React/dacce/src/components/header.jsx
@@ -3,6 +3,10 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+import { useState } from "react";
+import Sidemenu from "react-bootstrap/Offcanvas";
+import Button from "react-bootstrap/Button";
+
 // Import CSS
 import "../css/header.css";
 
@@ -11,10 +15,31 @@ import levelBadge from "../assets/level-badge.png";
 import starMedal from "../assets/star-medal.png";
 import profilePic from "../assets/profile.png";
 
-const Navbar = () => {
+// Import icons
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoPersonCircleOutline } from "react-icons/io5";
+import { PiRankingFill } from "react-icons/pi";
+import { LuHistory } from "react-icons/lu";
+import { IoMdSettings } from "react-icons/io";
+import { TbLogout } from "react-icons/tb";
+
+const Navbar = ({ currentView, onNavigate }) => {
+  const [show, setShow] = useState(false);
+
+  // Toggle the sidemenu
+  const menuToggle = () => setShow((prevShow) => !prevShow);
+
   return (
     <Container>
       <Row className="d-flex align-items-center px-5 py-3">
+        {/* Todo: fix the burger menu position */}
+        <Col xs={1} className="custom-nav-container">
+          {!show && (
+            <Button onClick={menuToggle} className="burger-menu-btn">
+              <RxHamburgerMenu />
+            </Button>
+          )}
+        </Col>
         <Col xs={2} className="custom-nav-container">
           <div className="custom-nav-item">
             <img src={levelBadge} alt="Level Badge" />
@@ -27,7 +52,7 @@ const Navbar = () => {
             <span>#19</span>
           </div>
         </Col>
-        <Col xs={6}></Col>
+        <Col xs={5}></Col>
         <Col
           xs={2}
           className="custom-nav-container custom-profile d-flex justify-content-end"
@@ -38,6 +63,50 @@ const Navbar = () => {
           </div>
         </Col>
       </Row>
+
+      {/* Side Menu */}
+      <Sidemenu show={show} onHide={() => setShow(false)} placement="start">
+        <Sidemenu.Header closeButton>
+          <Sidemenu.Title className="mx-5 fs-1 w-100">Menu</Sidemenu.Title>
+        </Sidemenu.Header>
+        <Sidemenu.Body className="d-flex flex-column justify-content-between h-100">
+          {/* Top Section */}
+          {/* Render "menu-top" if detected currentview is not dashboard */}
+          {currentView !== "dashboard" && (
+            <div className="menu-top">
+              <div>
+                <a className="fs-5 mx-5 menu-item" href="#profile">
+                  <IoPersonCircleOutline className="menu-icon" /> &nbsp; Profile
+                </a>
+                <a className="fs-5 mx-5 menu-item" href="#leaderboard">
+                  <PiRankingFill className="menu-icon" /> &nbsp; Leaderboard
+                </a>
+                <a className="fs-5 mx-5 menu-item" href="#history">
+                  <LuHistory className="menu-icon" /> &nbsp; History
+                </a>
+              </div>
+            </div>
+          )}
+
+          {/* Bottom Section */}
+          <div className="menu-bottom">
+            <a className="fs-5 mx-5 menu-item" href="#settings">
+              <IoMdSettings className="menu-icon" /> &nbsp; Settings
+            </a>
+            <a
+              className="fs-5 mx-5 menu-item"
+              href="#logout"
+              onClick={() => {
+                if (window.confirm("Are you sure you want to log out?")) {
+                  onNavigate("landing");
+                }
+              }}
+            >
+              <TbLogout className="menu-icon" /> &nbsp; Log Out
+            </a>
+          </div>
+        </Sidemenu.Body>
+      </Sidemenu>
     </Container>
   );
 };
