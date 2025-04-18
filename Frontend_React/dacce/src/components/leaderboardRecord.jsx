dd88c76c612ec8bb3352a3f8ddf3666e079efa31
Wen Kao <chiawenkao23@gmail.com>
2025-04-18 13:38:09 +1000

    Finish leaderboard frontend
    - win rate tbd

--- Frontend_React/dacce/src/components/leaderboardRecord.jsx
+++ Frontend_React/dacce/src/components/leaderboardRecord.jsx
@@ -1,12 +1,37 @@
-const LeaderboardRecord = ({ onNavigation }) => {
+// Import Bootstrap components
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+// Import CSS
+import "../css/leaderboard.css";
+
+const LeaderboardRecord = ({ data }) => {
+  const rowClass =
+    data.rank === 1
+      ? "custom-leaderboard-record first-place"
+      : data.rank === 2
+      ? "custom-leaderboard-record second-place"
+      : data.rank === 3
+      ? "custom-leaderboard-record third-place"
+      : "custom-leaderboard-record";
   return (
-    <Row height="50px">
-      <Col lg={2}>18</Col>
-      <Col lg={2}>Alexxxxx</Col>
-      <Col lg={4}><div></div></Col>
-      <Col lg={4}></Col>
-    </Row>
-  )
-}
+    <>
+      <Row className={`${rowClass} m-0 mb-4 mx-4`}>
+        <Col xs={2} md={2} lg={2}>
+          {data.rank}
+        </Col>
+        <Col xs={4} md={4} lg={2}>
+          {data.name}
+        </Col>
+        <Col xs={3} md={3} lg={4}>
+          {data.score}
+        </Col>
+        <Col xs={3} md={3} lg={4}>
+          {data.winRate}
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default LeaderboardRecord;
--- Frontend_React/dacce/src/views/leaderboard.jsx
+++ Frontend_React/dacce/src/views/leaderboard.jsx
@@ -1,27 +1,34 @@
-
+// Import Bootstrap components
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 // Import Components
-import InformationWindow from "../components/informationWindow";
+import LeaderboardRecord from "../components/leaderboardRecord";
 
 // Import CSS
 import "../css/leaderboard.css";
+
+// Import Assets
+import CancelBtn from "../assets/cancel.png";
 
 // Import contexts
 import { useState, useEffect } from "react";
 import { useWebSocket } from "../contexts/WebSocketContext";
 
 const Leaderboard = ({ onNavigate }) => {
-
   const [leaderboardData, setLeaderboardData] = useState([]);
   const { sendMessage } = useWebSocket();
 
   useEffect(() => {
-
-    const loginPayload = { type: "GET LEADERBOARD", message: "payload to get leaderboard" };
+    const loginPayload = {
+      type: "GET LEADERBOARD",
+      message: "payload to get leaderboard",
+    };
     sendMessage(loginPayload, (response) => {
       if (response.status === "SUCCESS") {
         setLeaderboardData(response.data);
-        console.log("set answer to: " + response.data.answer)
+        console.log("set answer to: " + response.data.answer);
       } else if (response.status === "EMPTY") {
         //const leaderboardData = "Unable to retrieve leaderboard results."
         setLeaderboardData("leadernoard empty");
@@ -31,31 +38,43 @@ const Leaderboard = ({ onNavigate }) => {
     });
   }, []);
 
-  
-
-  /*
-  const leaderboardData = [
-    { rank: 1, name: "Alice", score: 1200 },
-    { rank: 2, name: "Bob", score: 1150 },
-    { rank: 3, name: "Charlie", score: 1100 },
-    { rank: 4, name: "Dave", score: 1080 },
-    { rank: 5, name: "Eve", score: 1060 },
-    { rank: 6, name: "Alice", score: 1200 },
-    { rank: 7, name: "Bob", score: 1150 },
-    { rank: 8, name: "Charlie", score: 1100 },
-    { rank: 9, name: "Dave", score: 1080 },
-    { rank: 10, name: "Eve", score: 1060 },
-    { rank: 1, name: "Alice", score: 1200 },
-    { rank: 2, name: "Bob", score: 1150 },
-    { rank: 3, name: "Charlie", score: 1100 },
-    { rank: 4, name: "Dave", score: 1080 },
-    { rank: 5, name: "Eve", score: 1060 },
-    // Add more entries if needed
-  ];*/
-
   return (
     <>
-      <InformationWindow title="Leaderboard" onNavigate={onNavigate} data={leaderboardData}/>
+      <Container className="custom-leaderboard-container">
+        <div className="custom-trapezoid text-center">Leaderboard</div>
+
+        <img
+          src={CancelBtn}
+          alt="Close"
+          className="custom-close-btn"
+          onClick={() => onNavigate("dashboard")}
+        />
+
+        <Container fluid className="custom-leaderboard-data-container p-0">
+          <Row className="custom-leaderboard-title m-0 mx-5 pe-3 mb-3">
+            <Col xs={2} md={2} lg={2}>
+              Ranking
+            </Col>
+            <Col xs={4} md={4} lg={2}>
+              Player
+            </Col>
+            <Col xs={3} md={3} lg={4}>
+              Score
+            </Col>
+            <Col xs={3} md={3} lg={4}>
+              Win Rate
+            </Col>
+          </Row>
+          <Container
+            fluid
+            className="custom-leaderboard-record-container m-0 mt-4"
+          >
+            {leaderboardData.map((record, index) => (
+              <LeaderboardRecord data={record} key={index} />
+            ))}
+          </Container>
+        </Container>
+      </Container>
     </>
   );
 };
