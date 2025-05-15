5bb1034f77781244a09ad61cd234c82d90cde08f
Jack McCabe <jack.mccabe@uq.net.au>
2025-05-15 13:16:23 +1000

    removed old server folder

--- ServerClient/server-ws/utils/sendMessage.js
+++
@@ -1,6 +0,0 @@
-
-export async function okMessage(ws, data) {
-    const response = { status: "OK", message: data };
-    console.log('[Server] Sending response:', response.status + " " + data.type);
-    ws.send(JSON.stringify(response));
-}
